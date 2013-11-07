825360c8304dfad8e9bade4b1cd04428ea193598
Michael Matuzak <mmatuzak@gmail.com>
2013-11-06 20:13:16 -0800

    adding test for joy lib

---
+++ tests/joy.js
@@ -0,0 +1,16 @@
+var test = require('tap').test;
+var joyLib = require('../lib/joy');
+
+test("make sure the controller works", function (t) {
+    var expected = "ReadA:\n\tlda $4016\n\tand #%00000001\n\tbeq ReadADone\ntest\nReadADone:";
+
+    var output = joyLib.read('ReadA', function () { return 'test'; });
+    
+    t.equal(output, expected, "ReadA did not produce the expected output");
+
+    output = joyLib.read('ReadB', function () { return 'test'; });
+    expected = "ReadB:\n\tlda $4016\n\tand #%00000001\n\tbeq ReadBDone\ntest\nReadBDone:";
+
+    t.equal(output, expected, "ReadB did not produce the expected output");
+    t.end();
+});
