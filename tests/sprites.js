b820d1f718359fde769fe99640d1e78251b97b61
Michael Matuzak <mmatuzak@gmail.com>
2013-11-07 19:59:19 -0800

    fixing test

--- tests/sprites.js
+++ tests/sprites.js
@@ -4,10 +4,10 @@ var spriteLib = require('../lib/sprites');
 test("make sure addData is adding sprite data", function (t) {
     var expected = [
         'sprites:',
-        '\t.db $80, $32, $00, $80',
-        '\t.db $80, $33, $00, $88',
-        '\t.db $88, $34, $00, $80',
-        '\t.db $88, $35, $00, $88'].join('\n');
+        '\t.db $80, 32, $00, $80',
+        '\t.db $80, 33, $00, $88',
+        '\t.db $88, 34, $00, $80',
+        '\t.db $88, 35, $00, $88'].join('\n');
 
     // x, y, tile, attr
     spriteLib.addData({x: '80', 
