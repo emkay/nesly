189d716525d358783ddadd82a4f18ff797c7aa5f
Michael Matuzak <mmatuzak@gmail.com>
2013-11-07 21:34:56 -0800

    adding write function and addBgRow in order to write text to screen. this only works with the demo sprite sheet

---
+++ tests/bg.js
@@ -0,0 +1,10 @@
+var test = require('tap').test;
+var bgLib = require('../lib/bg');
+
+test("make sure we write hello world", function (t) {
+    bgLib.write('hello world');
+    t.equal('\t.db 17 14 21 21 24 36 32 24 27 21 13', 
+            bgLib.background[0],
+            "Expected hello world");
+    t.end()
+});
